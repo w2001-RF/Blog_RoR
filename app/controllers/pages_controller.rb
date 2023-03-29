6554d33413146d6aec32d48a7ce8a641b9cc8be9
unknown <w2001-RF>
2023-03-29 16:01:51 +0000

    add nom static html pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @titre = "Home"
   end
 
   def aide
+    @titre = "Aide"
   end
 
   def contact
+    @titre = "Contact"
   end
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,15 +4,18 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get pages_home_url
     assert_response :success
+    assert_select "title", "Home | Mon App"
   end
 
   test "should get aide" do
     get pages_aide_url
     assert_response :success
+    assert_select "title", "Aide | Mon App"
   end
 
   test "should get contact" do
     get pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | Mon App"
   end
 end
