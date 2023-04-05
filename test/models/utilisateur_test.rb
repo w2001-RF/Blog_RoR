7bbc95e54c28fdfc7666243578c7fee149a7816b
unknown <w2001-RF>
2023-04-05 15:29:56 +0000

    add test for sign in user

--- test/models/utilisateur_test.rb
+++ test/models/utilisateur_test.rb
@@ -4,4 +4,15 @@ class UtilisateurTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @utilisateur = Utilisateur.new(nom: "prenom1 Nom1",
+                   email: "prenom1@example.com")
+  end
+
+  test "nom foit etre present" do
+    @Utilisateur.nom = ""
+    assert_not @utilisateur.valid?
+  end
+
 end
