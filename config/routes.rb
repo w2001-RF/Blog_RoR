e59e62130dad86e531a2b9a60156a8fa8b49b6fc
unknown <w2001-RF>
2023-04-05 13:59:55 +0000

    Modify routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/aide'
-  get 'pages/contact'
+  root 'pages#home'
+  get 'aide'=> 'pge#aide'
+  get 'contact' => 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
