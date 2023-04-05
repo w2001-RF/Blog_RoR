f9daeb92997dd8d969c6c860409d4da92e219682
unknown <w2001-RF>
2023-04-05 14:03:42 +0000

    add footer to application

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'aide'=> 'pge#aide'
+  get 'aide'=> 'pages#aide'
   get 'contact' => 'pages#contact'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
