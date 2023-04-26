ca9c3f0ae258506f4072b2a396e5fd0349aaa194
unknown <w2001-RF>
2023-04-26 08:52:07 +0100

    add hashed  password

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem 'jbuilder', '~> 2.7'
 
 gem 'net-http'
 gem 'bootstrap-sass', '3.4.1'
-
+gem 'bcrypt',         '3.1.13'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
--- app/models/utilisateur.rb
+++ app/models/utilisateur.rb
@@ -4,4 +4,6 @@ class Utilisateur < ApplicationRecord
 
   VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL}
+  format: {with: VALID_EMAIL}
+  has_secure_password
 end
