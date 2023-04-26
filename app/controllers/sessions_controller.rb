334eec92564534a982ef41c03ae677e232737177
unknown <w2001-RF>
2023-04-26 11:06:40 +0100

    add sessions_helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     utilisateur = Utilisateur.find_by(email: params[:session][:email].downcase)
     if utilisateur && utilisateur.authenticate(params[:session][:password])
-      # A faire.
+      
     else
       flash[:danger] = 'Combinaison Email/Mot de passe invalide'
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,5 @@
 module SessionsHelper
+  def log_in(Utilisateur)
+    session[:utilisateur] = utilisateur.id
+  end
 end
