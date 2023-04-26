3e4ac5ee5b9e014fc09e77488abee8ffa9bdc86a
unknown <w2001-RF>
2023-04-26 11:03:06 +0100

    add sessions create method

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    utilisateur = Utilisateur.find_by(email: params[:session][:email].downcase)
+    if utilisateur && utilisateur.authenticate(params[:session][:password])
+      # A faire.
+    else
+      flash[:danger] = 'Combinaison Email/Mot de passe invalide'
+      render 'new'
+    end
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'pages#home'
   get 'aide'        => 'pages#aide'
   get 'contact'     => 'pages#contact'
   get 'inscription' => 'utilisateurs#new'
 
   resources :utilisateurs
+
+  get 'login'  => 'sessions#new'
+  post 'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
