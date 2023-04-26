f4cb9e1f8ded85002b46d00d39a89c0bcaead7ec
unknown <w2001-RF>
2023-04-26 11:33:30 +0100

    add logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,17 @@ class SessionsController < ApplicationController
   def create
     utilisateur = Utilisateur.find_by(email: params[:session][:email].downcase)
     if utilisateur && utilisateur.authenticate(params[:session][:password])
-      
+      log_in utilisateur
+      redirect_to utilisateur
     else
       flash[:danger] = 'Combinaison Email/Mot de passe invalide'
       render 'new'
     end
-    end
+  end
 
+  def destroy
+    log_out
+    redirect_to root_url
+  end
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,17 @@
 module SessionsHelper
-  def log_in(Utilisateur)
+  def log_in(utilisateur)
     session[:utilisateur_id] = utilisateur.id
   end
+
+  def utilisateur_courant
+    @utilisateur_courant = @utilisateur_courant || Utilisateur.find_by(id: session[:utilisateur_id])
+  end
+
+
+  def logged_in?
+    !utilisateur_courant.nil?
+  end
+
+  def log_out
+    
 end
